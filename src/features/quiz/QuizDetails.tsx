ae5e9c3007192a0c7d68beb113a8ecd2eda35663
Dacosta <dacsolo10@gmail.com>
2024-05-14 22:08:54 +0000

    removed a comment

--- src/features/quiz/QuizDetails.tsx
+++ src/features/quiz/QuizDetails.tsx
@@ -3,7 +3,6 @@ import { useQuiz } from "../../context/QuizContext";
 import QuizCompeted from "./QuizCompeted";
 import QuizInitial from "./QuizInitial";
 import QuizSession from "./QuizSession";
-// import QuizIntermediate from "./QuizIntermediate"
 
 function QuizDetails() {
   const {
